064e153e58a42b3a946a376df626439c0e2a61ce
furkananil <furkananil960@gmail.com>
2025-03-31 22:24:37 +0300

    KitapTuru Controller Oluşturuldu

--- Book-Rental-MVC/Utility/AppDbContext.cs
+++ Book-Rental-MVC/Utility/AppDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Book_Rental_MVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Book_Rental_MVC.Utility
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
     }
 }
