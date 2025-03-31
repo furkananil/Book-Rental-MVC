aeb51d921a7e1dbd62e1a514f9bea20285a59492
furkananil <furkananil960@gmail.com>
2025-03-31 19:23:04 +0300

    DbContext ve ConnectionString olusturuldu

---
+++ Book-Rental-MVC/Models/KitapTuru.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Rental_MVC.Models
+{
+    public class KitapTuru
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Ad { get; set; }
+    }
+}
---
+++ Book-Rental-MVC/Utility/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Book_Rental_MVC.Utility
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
