f65f490608b712070b95a6c34b586fbd785b2638
furkananil <furkananil960@gmail.com>
2025-04-01 18:51:52 +0300

    Bootswatch framework eklendi

--- Book-Rental-MVC/Controllers/KitapTuruController.cs
+++ Book-Rental-MVC/Controllers/KitapTuruController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Book_Rental_MVC.Models;
+using Book_Rental_MVC.Utility;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Book_Rental_MVC.Controllers
 {
     public class KitapTuruController : Controller
     {
+        private readonly AppDbContext _context;
+        public KitapTuruController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<KitapTuru> kitapTuruList = _context.KitapTurleri.ToList();
+            return View(kitapTuruList);
         }
     }
 }
