e3861c563f75c6dfb37e2d1f5de597cebd2e2570
furkananil <furkananil960@gmail.com>
2025-04-04 16:10:30 +0300

    KitapTuruRepository Oluşturuldu

---
+++ Book-Rental-MVC/Models/Concrete/KitapTuruRepository.cs
@@ -0,0 +1,24 @@
+﻿using Book_Rental_MVC.Models.Abstract;
+using Book_Rental_MVC.Utility;
+using System.Linq.Expressions;
+
+namespace Book_Rental_MVC.Models.Concrete
+{
+    public class KitapTuruRepository : Repository<KitapTuru>, IKitapTuruRepository
+    {
+        private AppDbContext _context;
+        public KitapTuruRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public void Guncelle(KitapTuru kitapTuru)
+        {
+            _context.Update(kitapTuru);
+        }
+
+        public void Kaydet()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
