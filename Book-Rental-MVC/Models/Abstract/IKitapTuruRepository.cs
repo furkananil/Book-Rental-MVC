cb69bfb299c63f139ca6cb7a809375a6db65844a
furkananil <furkananil960@gmail.com>
2025-04-04 15:57:06 +0300

    IKitapTuruRepository interface eklendi

---
+++ Book-Rental-MVC/Models/Abstract/IKitapTuruRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Book_Rental_MVC.Models.Abstract
+{
+    public interface IKitapTuruRepository : IRepository<KitapTuru>
+    {
+        void Guncelle(KitapTuru kitapTuru);
+        void Kaydet();
+    }
+}
