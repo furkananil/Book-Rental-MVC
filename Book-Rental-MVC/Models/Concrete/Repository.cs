b68b8c876a09cc4251c6534e0e361e5da8bb6cf2
furkananil <furkananil960@gmail.com>
2025-04-04 15:39:49 +0300

    Repository sınıfı, IRepository arayüzü ile implemente edildi

---
+++ Book-Rental-MVC/Models/Concrete/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Book_Rental_MVC.Models.Abstract;
+using Book_Rental_MVC.Utility;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Book_Rental_MVC.Models.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        internal DbSet<T> dbSet; 
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>(); // _context.KitapTurleri
+        }
+        public void Ekle(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filtre)
+        {
+            return dbSet.FirstOrDefault(filtre);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public void Sil(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void SilAralik(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
