3f49f1372e2c8cdd7157f2039f437e7cf474fc83
furkananil <furkananil960@gmail.com>
2025-04-04 15:07:54 +0300

    IRepository interface eklendi

---
+++ Book-Rental-MVC/Models/Abstract/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Book_Rental_MVC.Models.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        // T => KitapTuru
+        
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filtre);
+        void Ekle(T entity);
+        void Sil(T entity);
+        void SilAralik(IEnumerable<T> entities);
+    }
+}
